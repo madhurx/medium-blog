c3fdb01a3f5da0cbaf1d8f0eea3949eb681b4cdd
Madhur <madhurrgupta@gmail.com>
2024-04-09 23:40:04 +0530

    feat:middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
 	Bindings: {
@@ -9,6 +9,18 @@ const app = new Hono<{
 		JWT_SECRET: string;
 	};
 }>();
+
+app.use("/api/v1/blog/*", async (c, next) => {
+	const jwt = c.req.header("authorization") || "";
+	const token = jwt.split(" ")[1];
+	const response = await verify(token, c.env.JWT_SECRET);
+	if (response.id) {
+		await next();
+	} else {
+		c.status(403);
+		return c.json({ error: "unauthorized" });
+	}
+});
 
 app.post("/api/v1/signup", async (c) => {
 	const prisma = new PrismaClient({
