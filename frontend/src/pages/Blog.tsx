dd5e3abef66be08dcc463fc0a6a9c7fe8ad5a6c2
Madhur <madhurrgupta@gmail.com>
2024-04-11 23:27:07 +0530

    feat: blogCard

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import BlogCard from "../components/BlogCard";
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+	return (
+		<div className="flex justify-center">
+			<div className="max-w-xl min-w-96">
+				<BlogCard
+					author="author"
+					title="title"
+					content="content"
+					publishedDate={"20 Feb 2024"}
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default Blog
+export default Blog;
